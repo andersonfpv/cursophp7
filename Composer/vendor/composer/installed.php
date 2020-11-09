e06081d889aca69d0fbc507d93d872915bf4a46e
Anderson Felipe <anderson_fpv@yahoo.com.br>
2020-11-09 10:07:21 -0300

    Instalando o composer

---
+++ Composer/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '656d9a0e07988cb607b47ce929c994ac63e7e7cb',
+    'name' => 'and_fpv/composer',
+  ),
+  'versions' => 
+  array (
+    'and_fpv/composer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '656d9a0e07988cb607b47ce929c994ac63e7e7cb',
+    ),
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+    ),
+  ),
+);
