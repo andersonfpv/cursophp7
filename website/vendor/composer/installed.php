cd140e47bebc0eba486aa29ae6061edb37a8c954
Anderson Felipe <anderson_fpv@yahoo.com.br>
2020-11-11 23:12:19 -0300

    Slim Framework

---
+++ website/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'fa31ed2020f1c24f6961e7ffa222e58ec6f55ea1',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fa31ed2020f1c24f6961e7ffa222e58ec6f55ea1',
+    ),
+    'slim/slim' => 
+    array (
+      'pretty_version' => '2.0.0',
+      'version' => '2.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3e95c9abbc57a8f023ab4dacebcab9dae4d9f1f0',
+    ),
+  ),
+);
