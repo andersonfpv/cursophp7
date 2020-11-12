7bb411807722ee5361a0219ab7293614804f15f7
Anderson Felipe <anderson_fpv@yahoo.com.br>
2020-11-11 22:06:13 -0300

    RainTpl

---
+++ tpl/vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f3dd065d510486c651b8e4e86ee081a30d4a6070',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f3dd065d510486c651b8e4e86ee081a30d4a6070',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.1.8',
+      'version' => '6.1.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '917ab212fa00dc6eacbb26e8bc387ebe40993bc1',
+    ),
+    'rain/raintpl' => 
+    array (
+      'pretty_version' => '3.1.1',
+      'version' => '3.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'edee683bf242f40cc75bee46a78759f6c1589dca',
+    ),
+  ),
+);
