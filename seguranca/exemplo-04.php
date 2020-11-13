ff8e90c9beb9ced2119e4a5885a0a1ec4f778924
Anderson Felipe <anderson_fpv@yahoo.com.br>
2020-11-13 08:35:04 -0300

    Usando o reCaptcha para impedir robôs

---
+++ seguranca/exemplo-04.php
@@ -0,0 +1,9 @@
+<script src="https://www.google.com/recaptcha/api.js"></script>
+
+ <form action="cadastrar.php" method="post">
+
+ 	<div class="g-recaptcha" data-sitekey="6LeJgOIZAAAAAI5Exc9dP1cY4l2wFAo2CCYYsG8W"></div>
+ 	<input type="email" name="inputEmail">
+ 	<button type="submit">Enviar/button>
+
+ </form>
